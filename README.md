eacaf14593fbbb3a7483ab791910e3d240329cc0
Jeremy Siek <jsiek@129-79-241-203.dhcp-bl.indiana.edu>
2017-10-18 13:21:34 -0400

    lab preparation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# lab-quicksort
-Lab about generic quicksort
+# Lab: Generic QuickSort
+
+Implement quicksort in a way that is generic enough that it can be applied to an input 
+sequence that is represented as an `Array` or a `LinkedList`.
+That is, use the `Iter` interface. Also, the elements of the sequence do not have to
+be integers, but instead could be anything that implements the `java.util.Comparable`
+interface. Your goal should be to maintain the time complexity of quicksort.
+
+ 
---
+++ src/QuickSort.java
@@ -0,0 +1,17 @@
+import sequences.*;
+import static sequences.Algorithms.*;
+
+public class QuickSort {
+
+    /**
+     * TODO
+     * @param begin The position of the first element in the sequence to be sorted.
+     * @param end   The position that is one-past the last element in the sequence to be sorted.
+     * @param <E>   The element type for the sequence.
+     */
+    public static <E extends Comparable<? super E>>
+    void quicksort(Iter<E> begin, Iter<E> end) {
+        throw new UnsupportedOperationException();
+    }
+
+}
